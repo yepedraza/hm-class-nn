14439e449c3f2c5159efc8173255bbd33a2fd9de
yepedraza <yery.pedraza523@gmail.com>
2022-03-22 19:39:42 -0500

    Training method w/ abstraction

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,3 +1,4 @@
+from turtle import forward
 from dataset import Dataset
 import numpy as np
 
@@ -14,23 +15,28 @@ class NeuralNetwork:
         self.train_data = train_data
         self.layers_dim = layers_dim
 
-    def initialize_params(self):
+    def training(self):
+        self._initialize_params()
+        self._forward()
+
+    def _initialize_params(self):
         L = len(self.layers_dim)
         for l in range(0, L-1):
             self.params['W' + str(l+1)] = (np.random.rand(self.layers_dim[l],self.layers_dim[l+1]) * 2 ) - 1
             self.params['b' + str(l+1)] =  (np.random.rand(1,self.layers_dim[l+1]) * 2 ) - 1
 
-    def tanh(self, x, d=False):
+
+    def _tanh(self, x, d=False):
         if d:
             return 4/(np.exp(x)+np.exp(-x))**2
         else:
             return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
 
-    def forward_training(self):
+    def _forward(self):
         self.params['A0'] = self.train_data.x_train
 
         self.params['Z1'] = (self.params['A0']@self.params['W1']) + self.params['b1'] 
-        self.params['A1'] = self.tanh(self.params['Z1']) 
+        self.params['A1'] = self._tanh(self.params['Z1']) 
 
         self.params['Z2'] = (self.params['A1']@self.params['W2']) + self.params['b2'] 
-        self.params['A2'] = self.tanh(self.params['Z2']) 
+        self.params['A2'] = self._tanh(self.params['Z2']) 
