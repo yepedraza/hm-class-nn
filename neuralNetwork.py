ae64e45da603c942892f4491f1034c69a6f63471
yepedraza <yery.pedraza523@gmail.com>
2022-03-22 19:33:33 -0500

    tanh function and foward training

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,16 +1,17 @@
 from dataset import Dataset
 import numpy as np
 
-class NeuralNetwork(Dataset):
+class NeuralNetwork:
     layers_dim = []
     params = {}
     errors = []
-    epochs = int
-    lr = float
+    epochs = int 
+    alpha = float #Learning rate
     training = True
+    train_data = Dataset([],[])
 
-    def __init__(self, x_train, y_train, layers_dim):
-        super().__init__(x_train, y_train)
+    def __init__(self, train_data, layers_dim):
+        self.train_data = train_data
         self.layers_dim = layers_dim
 
     def initialize_params(self):
@@ -18,3 +19,18 @@ class NeuralNetwork(Dataset):
         for l in range(0, L-1):
             self.params['W' + str(l+1)] = (np.random.rand(self.layers_dim[l],self.layers_dim[l+1]) * 2 ) - 1
             self.params['b' + str(l+1)] =  (np.random.rand(1,self.layers_dim[l+1]) * 2 ) - 1
+
+    def tanh(self, x, d=False):
+        if d:
+            return 4/(np.exp(x)+np.exp(-x))**2
+        else:
+            return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+    def forward_training(self):
+        self.params['A0'] = self.train_data.x_train
+
+        self.params['Z1'] = (self.params['A0']@self.params['W1']) + self.params['b1'] 
+        self.params['A1'] = self.tanh(self.params['Z1']) 
+
+        self.params['Z2'] = (self.params['A1']@self.params['W2']) + self.params['b2'] 
+        self.params['A2'] = self.tanh(self.params['Z2']) 
