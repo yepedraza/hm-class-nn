243c5761423168ee5d8b4957f5467f8b7bbfb579
yepedraza <yery.pedraza523@gmail.com>
2022-03-23 11:06:37 -0500

    Created main file

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_gaussian_quantiles
+from dataset import Dataset
+from neuralNetwork import NeuralNetwork
+
+def run():
+    N  = 1000 #Creación de 1000 ejemplos
+    gaussian_quantiles= make_gaussian_quantiles(mean=None, cov=0.1, n_samples = N, n_features=2,
+                                                n_classes = 2, shuffle=True, random_state = None)
+
+    X, Y = gaussian_quantiles
+    Y = Y[:, np.newaxis]
+
+    neural_network = NeuralNetwork(Dataset(X,Y), [2, 4, 1])
+
+    neural_network.TrainingOFF(alpha=0.001, epochs=50)
+    neural_network.errors.pop()
+    #plt.plot(neural_network.errors)
+
+    neural_network.TrainingON(alpha=0.001, epochs=3)
+
+
+if __name__ == '__main__':
+    run()
